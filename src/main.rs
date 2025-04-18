887beaa9af1d4d03e4bf59dc29c03edf20a7f04a
Salvador Guzman <guzmansalv@gmail.com>
2025-04-18 04:58:15 -0700

    Add CLI argument parsing for SurrealDB import tool

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,51 @@
+use std::path::PathBuf;
+
+use clap::Parser;
 use surrealdb::Surreal;
 use surrealdb::engine::remote::ws::Ws;
 use surrealdb::opt::auth::Root;
+
+#[derive(Parser)]
+#[command(name = "Parallel Surrealdb Import")]
+#[command(bin_name = "parallel-surreal")]
+#[command(color = clap::ColorChoice::Always)]
+#[command(about = "Import data into SurrealDB in parallel")]
+#[command(author = "Salvador Guzman")]
+#[command(version = "1.0")]
+#[command(long_about = None)]
+struct Cli {
+    /// Sets a custom config file
+    #[arg(short, long)]
+    input: PathBuf,
+
+    // Name of surreal user
+    #[arg(short, long, default_value = "root")]
+    user: String,
+
+    // Password for surreal user
+    #[arg(short, long, default_value = "root")]
+    password: String,
+
+    // Name of surreal host
+    #[arg(short, long, default_value = "127.0.0.1")]
+    host: String,
+
+    // Port of surreal host
+    #[arg(short, long, default_value = "8000")]
+    port: String,
+
+    // Name of surreal namespace
+    #[arg(short, long, default_value = "test")]
+    ns: String,
+
+    // Name of surreal database
+    #[arg(short, long, default_value = "test")]
+    db: String,
+
+    // Number of threads
+    #[arg(short, long, default_value_t = 8)]
+    threads: usize,
+}
 
 #[tokio::main]
 async fn main() -> surrealdb::Result<()> {
